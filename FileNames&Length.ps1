47e083b7edf677697922d8a7bb4c44386198d1de
Kleinbp <57073756+Kleinbp@users.noreply.github.com>
2020-12-24 01:51:56 +0100

    Inital upload
    Im not into supporting so yeah...

---
+++ FileNames&Length.ps1
@@ -0,0 +1,15 @@
+﻿#Written by Philip Kleinberg
+
+
+
+#Location of the files you want to pull info from
+$FileLocation = "G:\Teamspeak\qtwebengine_locales"
+
+#actual command to generate table of names and file sizes
+$information = Get-ChildItem -Recurse -Path $FileLocation
+
+#Destination of Spreadsheet
+$Destination = 'C:\Users\Public\Documents\1st Output.csv'
+
+#Actual command to generate array of data
+$information | Format-Table -Property "Name","Size" | Out-File $Destination
